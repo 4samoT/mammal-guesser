a145a696c6b21d298a79dcaa1158a2f911db2019
Tomás Ferreira <82616776+4samoT@users.noreply.github.com>
2023-02-16 20:04:34 +0000

    Add files via upload

---
+++ mammal.pl
@@ -0,0 +1,511 @@
+:- dynamic animal/1.
+
+animal("Ouriço-Cacheiro").
+animal("Furão").
+animal("Rato").
+animal("Esquilo").
+animal("Chinchilla").
+animal("Porquinho da India").
+animal("Hamster").
+animal("Cão").
+animal("Coelho").
+animal("Gato").
+animal("Cabra").
+animal("Carneiro").
+animal("Boi").
+animal("Mula").
+animal("Burro").
+animal("Ponei").
+animal("Egua").
+animal("Cavalo").
+animal("Coelho").
+animal("Ovelha").
+animal("Porco").
+animal("Cão").
+animal("Camelo").
+animal("Dromedário").
+animal("Lhama").
+animal("Alpaca").
+animal("Bufalo").
+animal("Raposa").
+animal("Corço").
+animal("Gamo").
+animal("Rena").
+animal("VeadO").
+animal("Antilocapra").
+animal("Veado").
+animal("Rinoceronte").
+animal("Gazela").
+animal("Impala").
+animal("Órix").
+animal("Palanca").
+animal("Cobo").
+animal("Muflão").
+animal("Bongo").
+animal("Bufalo").
+animal("Girafa").
+animal("Cabra").
+animal("Camurça").
+animal("Ocapi").
+animal("Gnu").
+animal("Bisonte").
+animal("Preguiça").
+animal("Gibão").
+animal("Coala").
+animal("Facocero").
+animal("Lebre").
+animal("Ouriço-Cacheiro").
+animal("Cão-da-Pradaria").
+animal("Marmota").
+animal("Capivara").
+animal("Wombat").
+animal("Elefante").
+animal("Camelo").
+animal("Dromedário").
+animal("Gorila").
+animal("Canguru").
+animal("Wallaby").
+animal("Zebra").
+animal("Panda").
+animal("Elefante").
+animal("Hipopótamo").
+animal("Lémur").
+animal("Babuíno").
+animal("Saguíno").
+animal("Macaco").
+animal("Orangotango").
+animal("Chimpanzé").
+animal("Jupará").
+animal("Humano").
+animal("Anta").
+animal("Urso").
+animal("Javali").
+animal("Panda-Vermelho").
+animal("Raposa").
+animal("Esquilo").
+animal("Glutão").
+animal("Guaxinim").
+animal("Gambá").
+animal("Leirão").
+animal("Lince").
+animal("Chita").
+animal("Jaguar").
+animal("Leopardo").
+animal("Leão").
+animal("Tigre").
+animal("Ocelote").
+animal("Dingo").
+animal("Lobo").
+animal("Coiote").
+animal("Mangusto").
+animal("Suricata").
+animal("Gineta").
+animal("Arminho").
+animal("Marta").
+animal("Doninha").
+animal("Hiena").
+animal("Fossa").
+animal("Tatu").
+animal("Diabo-da-tasmânia").
+animal("Texugo").
+animal("Toupeira").
+animal("Porco-formigueiro").
+animal("Papa-formigas").
+animal("Morcego").
+animal("Morsa").
+animal("Leão-Marinho").
+animal("Foca").
+animal("Narval").
+animal("Orca").
+animal("Beluga").
+animal("Orca").
+animal("Baleia").
+animal("Golfinho").
+animal("Toninha").
+animal("Elefante-Marinho").
+animal("Peixe-Boi").
+animal("Dugongo").
+animal("Castor").
+animal("Ratão-do-Banhado").
+animal("Hipopotamo").
+animal("Capivara").
+animal("Ornitorrinco").
+animal("Lontra").
+
+:- dynamic node/4.
+
+node(1, "É totalmente terrestre?", 2, 113).
+node(2, "É doméstico?", 3, 31).
+node(3, "É de estimação?", 4, 13).
+node(4, "É um roedor?", 5, 11).
+node(5, "Tem espinhos?", "Ouriço-Cacheiro", 6).
+node(6, "Tem um corpo longo?", "Furão", 7).
+node(7, "Tem uma cauda?", 8, 10).
+node(8, "Tem uma cauda peluda?", 9, "Rato").
+node(9, "Come bolotas?", "Esquilo", "Chinchilla").
+node(10, "Tem a palavra India no nome?", "Porquinho da India", "Hamster").
+node(11, "Tem bigodes longos?", 12, "Cão").
+node(12, "Tem cauda curta?", "Coelho", "Gato").
+node(13, "É da quinta?", 14, 26).
+node(14, "Tem cornos?", 15, 18).
+node(15, "Tem barba?", "Cabra", 16).
+node(16, "Produz leite?", "Vaca", 17).
+node(17, "Dá lã?", "Carneiro", "Boi").
+node(18, "É usado para transporte de carga?", 19, 23).
+node(19, "Está relacionado com a falta de inteligência?", 20, 21).
+node(20, "É infertil?", "Mula", "Burro").
+node(21, "É de grande porte?", 22, "Ponei").
+node(22, "Produz Leite?", "Egua", "Cavalo").
+node(23, "É herbívoro?", 24, 25).
+node(24, "É de pequeno porte?", "Coelho", "Ovelha").
+node(25, "Tem uma cauda espiral?", "Porco", "Cão").
+node(26, "Vive no deserto?", 27, 28).
+node(27, "Tem mais que uma bossa?", "Camelo", "Dromedário").
+node(28, "Dá lã?", 29, 30).
+node(29, "Tem orelhas pontiagudas?", "Lhama", "Alpaca").
+node(30, "Tem cornos?", "Bufalo", "Raposa").
+node(31, "É herbívoro?", 32, 72).
+node(32, "Tem galhadas?", 33, 38).
+node(33, "As suas galhadas são pequenas?", "Corço", 34).
+node(34, "Tem cauda pequena?", 35, "Gamo").
+node(35, "Ambos os géneros tem galhadas?", "Rena", 36).
+node(36, "A sua pelagem é escura?", "Alce", 37).
+node(37, "Tem riscas brancas no pescoço?", "Antilocapra", "Veado").
+node(38, "Tem cornos?", 39, 53).
+node(39, "Tem chifres no nariz?", "Rinoceronte", 40).
+node(40, "Os seus cornos são grandes?", 41, 48).
+node(41, "Vive na savana?", 42, 46).
+node(42, "É conhecido pela sua velocidade?", 43, 44).
+node(43, 'É conhecido por fazer "Stotting"?', "Gazela", "Impala").
+node(44, "Os seus cornos são retos?", "Órix", 45).
+node(45, "Os seus cornos são curvos para trás?", "Palanca", "Cobo").
+node(46, "Os seus cornos são espirais?", "Muflão", 47).
+node(47, "Tem riscas no seu corpo?", "Bongo", "Bufalo").
+node(48, "Tem um pescoço longo?", "Girafa", 49).
+node(49, "Vive na montanha?", 50, 51).
+node(50, "Tem barba?", "Cabra", "Camurça").
+node(51, "Tem riscas no seu corpo?", 52, "Bisonte").
+node(52, "As suas riscas são brancas e pretas?", "Ocapi", "Gnu").
+node(53, "Tem menos de um metro de altura?", 54, 63).
+node(54, "Tem braços longos?", 55, 56).
+node(55, "Tem pouca mobilidade?", "Preguiça", "Gibão").
+node(56, "Está relacionado com as árvores?", "Coala", 57).
+node(57, "Tem presas?", "Facocero", 58).
+node(58, "Tem orelhas grandes?", "Lebre", 59).
+node(59, "É um roedor?", 60, "Wombat").
+node(60, "Tem cauda?", 61, "Capivara").
+node(61, "Tem espinhos?", "Ouriço-Cacheiro", 62).
+node(62, "A sua cauda é curta?", "Cão-da-Pradaria", "Marmota").
+node(63, "Vive no deserto?", 64, 66).
+node(64, "Tem dentes de marfim?", "Elefante", 65).
+node(65, "Tem mais que uma bossa?", "Camelo", "Dromedário").
+node(66, "Anda sobre duas patas?", 67, 69).
+node(67, "Tem cauda?", 68, "Gorila").
+node(68, "Possui pernas alongadas?", "Canguru", "Wallaby").
+node(69, "Tem cor branca e preta?", 70, 71).
+node(70, "Tem cascos?", "Zebra", "Panda").
+node(71, "Tem tromba?", "Elefante", "Hipopótamo").
+node(72, "É omnívero?", 73, 90).
+node(73, "É um primata?", 74, 81).
+node(74, "Possui o corpo totalmente revestido por pêlos?", 75, 80).
+node(75, "Possui o focinho achatado?", 76, "Lémur").
+node(76, "Tem cauda?", 77, 79).
+node(77, "A cauda é curta?", "Babuíno", 78).
+node(78, "É de pequeno porte?", "Saguíno", "Macaco").
+node(79, "Os seus pêlos são alaranjados?", "Orangotango", "Chimpanzé").
+node(80, "Tem cauda?", "Jupará", "Humano").
+node(81, "Tem peso elevado?", 82, 84).
+node(82, "Tem pêlo?", 83, "Anta").
+node(83, "Possui garras?", "Urso", "Javali").
+node(84, "Possui orelhas pontiagudas?", 85, 86).
+node(85, "Os seus pêlos são vermelhos e pretos?", "Panda-Vermelho", "Raposa").
+node(86, "Tem riscas negras faciais?", 87, 89).
+node(87, "Possui a cauda listrada?", "Guaxinim", 88).
+node(88, "Possui garras?", "Gambá", "Leirão").
+node(89, "É de pequeno porte?", "Esquilo", "Glutão").
+node(90, "É um felino?", 91, 97).
+node(91, "O pêlo possui manchas circulares(pintas, manchas,...)?", 92, 95).
+node(92, "Possui orelhas pontiagudas?", "Lince", 93).
+node(93, "Possui linhas negras à volta do focinho?", "Chita", 94).
+node(94, "Tem rosettas(marcas em forma de flôr) com pintas no meio?", "Jaguar", "Leopardo").
+node(95, "Tem juba?", "Leão", 96).
+node(96, "É de grande porte?", "Tigre", "Ocelote").
+node(97, "É um canídeo?", 98, 100).
+node(98, "É geralmente alaranjado?", "Dingo", 99).
+node(99, "Tem orelhas redondas?", "Lobo", "Coiote").
+node(100, "Possui o corpo longo?", 101, 108).
+node(101, "É de pequeno porte?", 102, 107).
+node(102, "Possui listras horizontais nas costas?", 103, 104).
+node(103, "A sua cauda é enorme?", "Mangusto", "Suricata").
+node(104, "Tem a ponta da cauda preta?", 105, 106).
+node(105, "Possui a cauda listrada?", "Gineta", "Arminho").
+node(106, "Possui uma camada de pêlo extra no inverno?", "Marta", "Doninha").
+node(107, "Tem pêlo?", "Hiena", "Fossa").
+node(108, "É de pequeno porte?", 109, 112).
+node(109, "Tem orelhas?", 110, "Toupeira").
+node(110, "Possui armadura?", "Tatu", 111).
+node(111, "Os seus bigodes são grandes?", "Diabo-da-tasmânia", "Texugo").
+node(112, "As suas orelhas são grandes?", "Porco-formigueiro", "Papa-formigas").
+node(113, "Voa?", "Morcego", 114).
+node(114, "Possui barbatanas?", 115, 127).
+node(115, "Está relacionado com os Polos?", 116, 121).
+node(116, "Tem dentes grandes?", "Morsa", 117).
+node(117, "Tem orelhas?", "Leão-marinho", 118).
+node(118, "Consegue viver fora de água?", "Foca", 119).
+node(119, "Possui chifre?", "Narval", 120).
+node(120, "É preto?", "Orca", "Beluga").
+node(121, "Tem barbatana dorsal?", 122, 125).
+node(122, "É preto?", "Orca", 123).
+node(123, "É o maior animal da Terra?", "Baleia", 124).
+node(124, "O seu focinho é comprido?", "Golfinho", "Toninha").
+node(125, "Consegue viver fora de água?", "Elefante-Marinho", 126).
+node(126, "É herbívoro?", "Peixe-Boi", "Dugongo").
+node(127, "Está relacionado com a madeira?", "Castor", 128).
+node(128, "É herbívoro?", 129, 131).
+node(129, "Tem cauda longa?", "Ratão-do-Banhado", 130).
+node(130, "É de grande porte?", "Hipopotamo", "Capivara").
+node(131, "Poem ovos?", "Ornitorrinco", "Lontra").
+
+:- dynamic userPath/3.
+
+
+:- dynamic yes/1.
+
+yes("Sim").
+
+:- dynamic input/1.
+
+input(A) :-
+    nl,
+    read(B),
+    atom_length(B, C),
+    D is C+ -1,
+    sub_atom(B, 0, 1, _, E),
+    sub_atom(B, 1, D, _, F),
+    string_upper(E, G),
+    atom_concat(G, F, H),
+    atom_string(H, A).
+
+:- dynamic start/0.
+
+start :-
+    write("\t\tBem vindo ao Mammal Guesser!"),
+    nl,
+    write("Pense num animal mamífero e eu vou tentar adivinhar, "),
+    nl,
+    write("Ah, e não se esqueça de escrever sempre em minúscula com acentuação entre áspas"),
+    nl,
+    nl,
+    ask(0, 1),
+    retractall(userPath(_, _, _)),
+    save,
+    !.
+
+:- dynamic ask/2.
+
+ask(A, B) :-
+    animal(B),
+    end(A, B).
+ask(_, A) :-
+    (   node(A, B, _, _),
+        write(B),
+        write(" "),
+        input(C),
+        yes(C)
+    ->  node(A, _, D, _),
+        assert(userPath(A, D, true)),
+        ask(A, D)
+    ;   node(A, _, _, E),
+        assert(userPath(A, E, false)),
+        ask(A, E)
+    ).
+
+:- dynamic end/1.
+
+
+end(A, B) :-
+    (   write(B),
+        nl,
+        write("É este animal? "),
+        input(C),
+        yes(C)
+    ->  write("Eu sabia!")
+    ;   verify(A, B)
+    ).
+
+:- dynamic verify/1.
+
+
+verify(A, B) :-
+    write("Em que animal estavas a pensar? "),
+    input(C),
+    verify(A, B, C).
+
+verify(A, B, C) :-
+    (   animal(C)
+    ->  path(A, B, C)
+    ;   add(A, B, C)
+    ).
+
+:- dynamic path/2.
+
+
+path(A, B, C) :-
+    write("--- "),
+    write(C),
+    write(" ---"),
+    nl,
+    showAllPath(C),
+    write("--- "),
+    write("As tuas respostas"),
+    write(" ---"),
+    nl,
+    showUserPath(1),
+    write("Enganaste-te? "),
+    input(D),
+    path(A, B, C, D).
+
+path(A, B, C, D) :-
+    (   yes(D)
+    ->  write("Eu sabia, eu estou sempre certo!")
+    ;   add(A, B, C)
+    ).
+
+:- dynamic showAllPath/1.
+
+showAllPath(A) :-
+    getAllPath(A, B),
+    showAll(B, A, 1).
+
+:- dynamic getAllPath/2.
+
+getAllPath(A, B) :-
+    findall(C,
+            (   node(C, _, A, _)
+            ;   node(C, _, _, A)
+            ),
+            B).
+
+:- dynamic showAll/2.
+
+
+showAll([], _, _).
+showAll([A|B], C, D) :-
+    write("Caminho "),
+    write(D),
+    nl,
+    showFirst(A, C),
+    showPath(A),
+    nl,
+    E is D+1,
+    showAll(B, C, E).
+
+:- dynamic showFirst/2.
+
+showFirst(A, B) :-
+    node(A, C, _, B),
+    write(C),
+    write(" Não"),
+    nl.
+showFirst(A, B) :-
+    node(A, C, B, _),
+    write(C),
+    write(" Sim"),
+    nl.
+
+:- dynamic showPath/1.
+
+showPath(1).
+showPath(A) :-
+    (   node(B, C, A, _)
+    ->  write(C),
+        write(" Sim"),
+        nl,
+        showPath(B)
+    ;   node(D, E, _, A),
+        write(E),
+        write(" Não"),
+        nl,
+        showPath(D)
+    ).
+
+:- dynamic showUserPath/1.
+
+showUserPath(A) :-
+    animal(A),
+    write(A),
+    nl,
+    !.
+showUserPath(A) :-
+    (   node(A, B, _, _),
+        userPath(A, C, D),
+        showUserPath(C),
+        write(B),
+        write(" "),
+        D==true
+    ->  write("Sim"),
+        nl
+    ;   write("Não"),
+        nl
+    ).
+
+:- dynamic add/3.
+
+add(A, B, C) :-
+    write("Não tenho esse animal, faça uma pergunta que o animal "),
+    write(C),
+    write(" tenha e que o animal "),
+    write(B),
+    write(" não tenha: "),
+    input(D),
+    add(A, B, C, D).
+
+:- dynamic add/4.
+
+add(A, B, C, D) :-
+    node(A, E, B, F),
+    getId(G),
+    retract(node(A, E, B, F)),
+    assert(node(A, E, G, F)),
+    assert(node(G, D, C, B)),
+    assert(animal(C)).
+add(A, B, C, D) :-
+    node(A, E, F, B),
+    getId(G),
+    retract(node(A, E, F, B)),
+    assert(node(A, E, F, G)),
+    assert(node(G, D, C, B)),
+    assert(animal(C)).
+
+:- dynamic getId/1.
+
+getId(A) :-
+    findall(B, animal(B), C),
+    length(C, D),
+    A is D+1.
+
+:- dynamic save/0.
+
+save :-
+    tell('mammal.pl'),
+    listing(animal),
+    listing(node),
+    listing(userPath),
+    listing(yes),
+    listing(input),
+    listing(start),
+    listing(ask),
+    listing(end),
+    listing(verify),
+    listing(path),
+    listing(showAllPath),
+    listing(getAllPath),
+    listing(showAll),
+    listing(showFirst),
+    listing(showPath),
+    listing(showUserPath),
+    listing(add),
+    listing(getId),
+    listing(save),
+    told.
+
